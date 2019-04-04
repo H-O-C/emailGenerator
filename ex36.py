006599cd126e723475713e5c579376ee04f8d6d2
Malik Smith <smithmalik2468@gmail.com>
2019-04-04 13:33:50 -0700

    Update ex36.py

--- ex36.py
+++ ex36.py
@@ -1,13 +1,14 @@
+# Import random, will be used for random.randint()
 import random
 
-name = input("first and last name ")
+firstName = input("Enter your first name: ")
 
-namename = name.replace(" ", "")
-
-num = random.randint(10, 1999)
+lastName = input("Enter your last name: ")
+# The random part of the email generator will be the numbers. It could be either one, two, three, or four digits long. 
+num = random.randint(1, 1199)
 
 gmail = "@gmail.com"
-
-email = namename + str(num) + gmail
-
+#combine variables starting with lastName + "."(period) + firstName + num + gmail
+email = lastName + "." + firstName + num + gmail
+# use .lower() function to make entire email lower case no matter the input.
 print(email.lower())
